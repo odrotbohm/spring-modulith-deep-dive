6d913e3be1909ca12c290c6176a88ec602b1c1bd
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2024-05-01 08:49:59 +0200

    Create academy workshop folder structure.

--- prep4educates.sh
+++ prep4educates.sh
@@ -7,7 +7,11 @@ rm -rf $basedir
 # Scan folders starting with two digits
 for dir in [0-9][0-9]*/; do
 
-    mkdir -p "${basedir}/${dir}"
+    # Spring Academy Course expected folder structure
+    target="${basedir}/workshops/${dir}workshop/content"
+
+    # Create folder
+    mkdir -p $target
 
     # Find all AsciiDoc files recursively
     find $dir -name "*.adoc" -type f | while read -r file; do
@@ -19,7 +23,7 @@ for dir in [0-9][0-9]*/; do
 
         # Convert DocBook to Markdown using Pandoc
         # -s to keep the primary headline
-        pandoc -s -f docbook -t markdown -o "./target/educates/${dir}/${filename}.md"
+        pandoc -s -f docbook -t markdown -o "${target}/${filename}.md"
 
     done
 
